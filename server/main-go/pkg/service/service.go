0dfe799c168d7b327155ee4b20a8f2e7f31fb20e
Willyamdevon <dezhintern@gmail.com>
2024-06-15 23:11:34 +0300

    small fixion

--- server/main-go/pkg/service/service.go
+++ server/main-go/pkg/service/service.go
@@ -7,16 +7,9 @@ import (
 type Authorization interface {
 }
 
-type TodoList interface {
-}
-
-type TodoItem interface {
-}
 
 type Service struct {
 	Authorization
-	TodoList
-	TodoItem
 }
 
 func NewService(repo *repository.Repository) *Service {

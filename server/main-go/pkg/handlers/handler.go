d93679c686651987dae2a80938cdef57a6ddb008
Willyamdevon <dezhintern@gmail.com>
2024-06-16 02:57:20 +0300

    add rotes (I hope all)

--- server/main-go/pkg/handlers/handler.go
+++ server/main-go/pkg/handlers/handler.go
@@ -20,6 +20,25 @@ func (h *Handler) InitRoutes() *gin.Engine {
 	{
 		auth.POST("/sign-up")
 		auth.POST("/sign-in")
+		auth.GET("/sign-out")
+	}
+	user := router.Group("/user")
+	{
+		user.GET("/user-info")
+		user.PUT("/update-info")
+		user.PUT("/update-password")
+		user.GET("/avatar")
+	}
+	ai := router.Group("/ai")
+	{
+		ai.GET("/show-chats")
+		ai.GET("/show-chat")
+		ai.DELETE("/delete-chat")
+	}
+	chat := router.Group("/chat")
+	{
+		chat.POST("/post-message")
+		chat.DELETE("/clear-chat")
 	}
 
 	return router

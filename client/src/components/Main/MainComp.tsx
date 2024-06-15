b0a2673ba8eb30a10dd386a97e6a266bb39444c0
Garleen <garleenn@gmail.com>
2024-06-15 23:02:43 +0300

    Структура + базовые компоненты и настройки

---
+++ client/src/components/Main/MainComp.tsx
@@ -0,0 +1,10 @@
+
+
+export function MainComp() {
+
+	return (
+		<>
+			<h1 className="text-3xl">jghvj</h1>
+		</>
+	)
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+
+import { MainPage } from './pages/Main/MainPage'; 
+import { ErrorPage } from './pages/ErrorPage/ErrorPage';
+
 import './index.css'
+
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainPage />,
+    errorElement: <ErrorPage />
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+     <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+     </QueryClientProvider>
   </React.StrictMode>,
 )

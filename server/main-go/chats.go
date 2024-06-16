84df246a4c48a368052ee9939b73415f32673bed
Willyamdevon <dezhintern@gmail.com>
2024-06-16 10:10:31 +0300

    add structs fro binding json

--- server/main-go/chats.go
+++ server/main-go/chats.go
@@ -1,1 +1,22 @@
 package main_go
+
+type Message struct {
+	Id       string `json:"-" db:"id"`
+	Username string `json:"username"`
+	Message  string `json:"message"`
+	Chat_id  string `json:"chat_id"`
+}
+
+type Chat struct {
+	Id      string `json:"-" db:"id"`
+	Type    string `json:"type"`
+	Title   string `json:"title"`
+	User_id string `json:"user_id"`
+}
+
+type User struct {
+	Id       string `json:"-" db:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
